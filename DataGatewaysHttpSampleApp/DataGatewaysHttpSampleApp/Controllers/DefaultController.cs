78c2415811e8b2553e623b15c53078905ec0882a
Charlie Kovacs <charles.kovacs@blueprism.com>
2021-01-27 13:37:54 -0600

    add sample HTTP app

---
+++ DataGatewaysHttpSampleApp/DataGatewaysHttpSampleApp/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataGatewaysHttpSampleApp.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage EchoRequest()
+        {
+            string receivedMessage = Request.Content.ReadAsStringAsync().Result;
+            var returnedMessage = Request.CreateResponse(HttpStatusCode.OK, receivedMessage);
+            return returnedMessage;
+        }
+    }
+}
---
+++ DataGatewaysHttpSampleApp/DataGatewaysHttpSampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataGatewaysHttpSampleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataGatewaysHttpSampleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22b24281-4135-407d-b192-74dfb3d3ae81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
